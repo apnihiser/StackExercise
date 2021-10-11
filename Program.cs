0d83d63d897e0dd45b743abc55c78e7c67364226
Adam <apnihiser@gmail.com>
2021-10-11 16:28:15 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System;
+
+namespace SeactionFourStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int command;
+                bool result;
+                string input;
+                string msg = "initializing...";
+                Stack stack = new Stack();
+
+                do
+                {
+                    RenderMainMenu(msg);
+
+                    input = Console.ReadLine();
+
+                    result = int.TryParse(input, out command);
+
+                    if (result == true)
+                    {
+                        switch (command) 
+                        {
+                            case 1:
+                                msg = $"Contents of stack: {stack.ReadStack()}";
+                                break;
+                            case 2:
+                                stack.Push(1);
+                                msg = $"##### Pushing Test data (integer 1)...\nStack size: {stack.StackLength()}.";
+                                break;
+                            case 3:
+                                msg = $"##### Popped: {stack.Pop()}.\nStack size: {stack.StackLength()}.";
+                                break;
+                            case 4:
+                                stack.Clear();
+                                msg = $"##### Clearing stack...\nStack size: {stack.StackLength()}.";
+                                break;
+                            case 5:
+                                msg = "##### Prepare for a crash...";
+                                stack.Push(null);
+                                break;
+                            default:
+                                msg = "##### Exiting program...";
+                                command = -1;
+                                break;
+                        }
+                    }
+                    else if(result == false)
+                    {
+                        Console.WriteLine("Invalid command, please check your entry and try again.");
+                    }
+                } while (command != -1);
+            }
+            catch (InvalidOperationException err)
+            {
+                Console.WriteLine($"Error : {err}");
+            }
+        }
+
+        public static void RenderMainMenu(string msg)
+        {
+            msg = (msg == null) ? "" : msg;
+
+            Console.Clear();
+            Console.WriteLine("CALL STACK SIMULATION PROGRAM");
+            Console.WriteLine("\nPress 1: Read the contents of the stack... \n" +
+            "Press 2: Push an object on the stack...      \n" +
+            "Press 3: Pop an object from the stack...     \n" +
+            "Press 4: Remove all objects from the stack...\n" +
+            "Press 5: Attempt to add null to the stack... \n" +
+            "Press 0: Exit program...                     \n");
+            Console.WriteLine($"Status Message: {msg}\n");
+            Console.Write(":~$ ");
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SeactionFourStack
+{
+    class Stack
+    {
+        // Stack operates LIFO. Last object in the List will be the latest object added from push(), then the first to pop().
+        // all items can be upcast to objects.
+        // null shouldn't be added. instead cause InvalidOperationException
+        // Pop method called on an empty stack should cause InvalidOperationException.
+        // Clear will set _stack to end up with 0 elements.
+
+        private readonly List<object> _stack = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("Null values cannot be added to stack.");
+
+            this._stack.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (_stack.Count == 0)
+                throw new InvalidOperationException("Stack empty, nothing to remove.");
+
+            var lastElement = this._stack[_stack.Count - 1];
+            _stack.Remove(this._stack[_stack.Count - 1]);
+            return lastElement;
+        }
+
+        public string ReadStack()
+        {
+            var msg = new List<string>();
+
+            foreach(object obj in this._stack)
+            {
+                msg.Add(obj.ToString());
+            }
+
+            return String.Join(", ", msg.ToArray());
+        }
+
+        public int StackLength()
+        {
+            return _stack.Count;
+        }
+
+        public void Clear()
+        {
+                this._stack.RemoveAll(obj => obj == obj);
+        }
+    }
+}
