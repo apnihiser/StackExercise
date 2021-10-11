e38917fe4f8729f6fa02405d1ccddd5f74ed1994
Adam <apnihiser@gmail.com>
2021-10-11 16:33:01 -0400

    corrected misspelling.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System;
 
-namespace SeactionFourStack
+namespace SectionFourStack
 {
     class Program
     {
--- Stack.cs
+++ Stack.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace SeactionFourStack
+namespace SectionFourStack
 {
     class Stack
     {
